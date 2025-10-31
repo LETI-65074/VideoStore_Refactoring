e86f9eb0751e39e9bc12a358e4a71e5290b0d963
LETI-122606 <emssa@iscte-iul.pt>
2025-10-31 00:44:37 +0000

    Na classe Customer, copiei o método statement e colei abaixo e mudei o nome do método copiado para htmlStatement. Depois adaptei o contéudo para html. Substituí o cabeçalho textual por tags html, adicionei <br> no fim de cada linha do filme e adicionei a secção final também com html.

--- src/v3/Customer.java
+++ src/v3/Customer.java
@@ -27,7 +27,7 @@ public class Customer
         // header
         String result = "Rental Record for " + getName() + "\n";
 
-        for (Rental each: _rentals)
+        for (Rental each : _rentals)
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
 
         // add footer lines
@@ -36,10 +36,29 @@ public class Customer
         return result;
     }
 
+    public String htmlStatement()
+    {
+        // header
+        String result = "<font size=\"5\" face=\"Georgia, Arial, Garamond\" color=\"green\">\n";
+        result += "<h2>Rental Record for <i>" + getName() + "</i></h2>\n";
+
+        result += "<ul>\n";
+        for (Rental each : _rentals)
+            result += "\t<li>" + each.getMovie().getTitle() + "\t" + each.getAmount()+"\n";
+        result += "</ul>\n";
+
+        // add footer lines
+        result += "Amount owed is " + getTotalAmount() + "<br>\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points<br>\n";
+        result += "</font>\n";
+
+        return result;
+    }
+
     public int getTotalFrequentRenterPoints()
     {
         int frequentRenterPoints = 0;
-        for (Rental each: _rentals)
+        for (Rental each : _rentals)
             frequentRenterPoints += each.getFrequentRentalPoints();
         return frequentRenterPoints;
     }
@@ -47,7 +66,7 @@ public class Customer
     public double getTotalAmount()
     {
         double totalAmount = 0;
-        for (Rental each: _rentals)
+        for (Rental each : _rentals)
             totalAmount += each.getAmount();
         return totalAmount;
     }
