96d37fb6842ac9d57ff8507b7317a3033e4b2c9e
LETI-122606 <emssa@iscte-iul.pt>
2025-10-31 00:33:38 +0000

    Abri o Customer.java e localizei o código no statement() que calcula o totalAmount. Selecionei este código, que continha o loop e fiz Refactor, depois Extract, Method e de seguida nomeei getTotalAmount(). Fiz o mesmo para frequentRenterPoints, Refactor, Extract, Method e nomeei getTotalFrequentRenterPoints(). Substituí as variáveis temporárias no statement() pelas chamadas aos métodos extraídos. Não usei o Refactor e Inline Variable para remover as temporárias.

--- src/v3/Customer.java
+++ src/v3/Customer.java
@@ -4,45 +4,51 @@ import java.util.Vector;
 
 public class Customer
 {
-	private String			_name;
-	private Vector<Rental>	_rentals	= new Vector<Rental>();
+    private String			_name;
+    private Vector<Rental>	_rentals	= new Vector<Rental>();
 
-	public Customer(String _name)
-	{
-		this._name = _name;
-	}
+    public Customer(String _name)
+    {
+        this._name = _name;
+    }
 
-	public void addRental(Rental arg)
-	{
-		_rentals.addElement(arg);
-	}
+    public void addRental(Rental arg)
+    {
+        _rentals.addElement(arg);
+    }
 
-	public String getName()
-	{
-		return _name;
-	}
+    public String getName()
+    {
+        return _name;
+    }
 
-	public String statement()
-	{
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
+    public String statement()
+    {
+        // header
+        String result = "Rental Record for " + getName() + "\n";
 
-		// header
-		String result = "Rental Record for " + getName() + "\n";
-		
-		for (Rental each: _rentals)
-		{
-			frequentRenterPoints += each.getFrequentRentalPoints();
+        for (Rental each: _rentals)
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
 
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
-			totalAmount += each.getAmount();
-		}
+        // add footer lines
+        result += "Amount owed is " + getTotalAmount() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
+        return result;
+    }
 
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
+    public int getTotalFrequentRenterPoints()
+    {
+        int frequentRenterPoints = 0;
+        for (Rental each: _rentals)
+            frequentRenterPoints += each.getFrequentRentalPoints();
+        return frequentRenterPoints;
+    }
 
+    public double getTotalAmount()
+    {
+        double totalAmount = 0;
+        for (Rental each: _rentals)
+            totalAmount += each.getAmount();
+        return totalAmount;
+    }
 }
